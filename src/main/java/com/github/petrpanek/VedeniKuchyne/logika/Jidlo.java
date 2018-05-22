82fc8ee5744551ff6de3bfef81b28de6f288d5c3
MarekP77 <posm04@vse.cz>
2018-05-22 13:29:31 +0200

    Přídání třídy - Jídlo, Recept

--- src/main/java/com/github/petrpanek/VedeniKuchyne/logika/Jidlo.java
+++ src/main/java/com/github/petrpanek/VedeniKuchyne/logika/Jidlo.java
@@ -1,5 +1,60 @@
 package com.github.petrpanek.VedeniKuchyne.logika;
 
+import java.util.Date;
+
 public class Jidlo {
+	private String nazev;
+	private int pocetPorci;
+	private int idJidla;
+	private Date casVareni;
+
+
+public Jidlo(String nazev, int pocetPorci, int idJidla, Date casVareni){
+	this.nazev = nazev;
+	this.pocetPorci = pocetPorci;
+	this.idJidla = idJidla;
+	this.casVareni = casVareni;
+}
+
+
+public int getIdJidla() {
+	return idJidla;
+}
+
+
+public void setIdJidla(int idJidla) {
+	this.idJidla = idJidla;
+}
+
+
+public String getNazev() {
+	return nazev;
+}
+
+
+public void setNazev(String nazev) {
+	this.nazev = nazev;
+}
+
+
+public int getPocetPorci() {
+	return pocetPorci;
+}
+
+
+public void setPocetPorci(int pocetPorci) {
+	this.pocetPorci = pocetPorci;
+}
+
+
+public Date getCasVareni() {
+	return casVareni;
+}
+
+
+public void setCasVareni(Date casVareni) {
+	this.casVareni = casVareni;
+}
 
 }
+
--- src/main/java/com/github/petrpanek/VedeniKuchyne/logika/Recept.java
+++ src/main/java/com/github/petrpanek/VedeniKuchyne/logika/Recept.java
@@ -1,5 +1,69 @@
 package com.github.petrpanek.VedeniKuchyne.logika;
 
 public class Recept {
+	private String nazev;
+	private String postup;
+	private double dobaPripravy;
+	private int idReceptu;
+	private double obtiznost;
+
+
+public Recept(String nazev, String postup, double dobaPripravy, int idReceptu, double obtiznost){
+	this.nazev = nazev;
+	this.postup = postup;
+	this.dobaPripravy = dobaPripravy;
+	this.idReceptu = idReceptu;
+	this.obtiznost = obtiznost;
+}
+
+
+public double getObtiznost() {
+	return obtiznost;
+}
+
+
+public void setObtiznost(double obtiznost) {
+	this.obtiznost = obtiznost;
+}
+
+
+public String getNazev() {
+	return nazev;
+}
+
+
+public void setNazev(String nazev) {
+	this.nazev = nazev;
+}
+
+
+public String getPostup() {
+	return postup;
+}
+
+
+public void setPostup(String postup) {
+	this.postup = postup;
+}
+
+
+public double getDobaPripravy() {
+	return dobaPripravy;
+}
+
+
+public void setDobaPripravy(double dobaPripravy) {
+	this.dobaPripravy = dobaPripravy;
+}
+
+
+public int getIdReceptu() {
+	return idReceptu;
+}
+
+
+public void setIdReceptu(int idReceptu) {
+	this.idReceptu = idReceptu;
+}
 
 }
