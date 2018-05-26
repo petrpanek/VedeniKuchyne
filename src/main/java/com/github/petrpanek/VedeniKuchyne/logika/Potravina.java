8d4bf99ec7ba7868181737ed5da7f79300874c79
tiboy <tiboyv@seznam.cz>
2018-05-26 18:11:21 +0200

    udělána Potravina a sklad

--- src/main/java/com/github/petrpanek/VedeniKuchyne/logika/Potravina.java
+++ src/main/java/com/github/petrpanek/VedeniKuchyne/logika/Potravina.java
@@ -1,5 +1,19 @@
 package com.github.petrpanek.VedeniKuchyne.logika;
 
-public class Potravina {
 
+public class Potravina {
+    
+	private String nazev;
+     
+    public Potravina(String nazev) {
+    	this.nazev = nazev;
+    }
+    
+    public String getNazev() {
+    	return nazev;
+    }
+    
+    public void setNazev(String nazev) {
+    	this.nazev = nazev;
+    }
 }
--- src/main/java/com/github/petrpanek/VedeniKuchyne/logika/Sklad.java
+++ src/main/java/com/github/petrpanek/VedeniKuchyne/logika/Sklad.java
@@ -1,5 +1,53 @@
 package com.github.petrpanek.VedeniKuchyne.logika;
 
-public class Sklad {
+import java.util.HashMap;
+import java.util.Map;
 
+
+public class Sklad {
+   
+	private Map<String, Potravina> sklad; 
+	
+    public Sklad () {
+    	sklad = new HashMap<String, Potravina>();
+    }
+    /**
+     * Vloží potravinu do skladu
+     *
+     *@param  potravina  instance potraviny, která se má vložit
+     */
+   public void vlozPotravinu (Potravina potravina) {
+     sklad.put(potravina.getNazev(),potravina);
+    }
+     /**
+     * Vrací  mapu portravin, které jsou ve skladu
+
+     *@return            mapa potravin
+     */
+    public Map<String, Potravina> getSklad() {
+		return sklad;
+	}
+     /**
+     * Hledá věc daného jména a pokud je ve skladu, tak ji vrátí a vymaže ze seznamu
+
+     *@param  název   Jméno potraviny
+     *@return            potravina nebo
+     *                   hodnota null, pokud tam potravina daného názvu není 
+     */
+    public Potravina odstranPotravinu (String nazevPotraviny) {
+        Potravina potravina = null;
+        if (sklad.containsKey(nazevPotraviny)) {
+        	potravina = sklad.get(nazevPotraviny);
+            sklad.remove(nazevPotraviny);
+        }   
+        return potravina;
+    }
+    
+    public Potravina vratPotravinu (String nazevPotraviny) {
+        Potravina potravina = null;
+        if (sklad.containsKey(nazevPotraviny)) {
+        	potravina = sklad.get(nazevPotraviny);
+        }   
+        return potravina;
+    }
 }
